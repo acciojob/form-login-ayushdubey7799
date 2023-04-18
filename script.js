594a74d71e23773fc0a494e475c24d6a3e1ae6f5
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-04-18 15:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+  let fname = document.getElementsByName('fname');
+	let lname = document.getElementsByName('lname');
+    alert(fname[0].value+' '+lname[0].value);
 }
+
+
